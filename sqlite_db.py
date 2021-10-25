50ee31bcf3e65d8af261b8f54334bb900da00689
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-24 21:03:49 -0400

    Added multiple functions for modifying sqlite SQL database

--- sqlite_db.py
+++ sqlite_db.py
@@ -24,8 +24,6 @@ def insert(item, quantity, price):
     conn.commit()
     conn.close()
 
-insert('Water Glass', 8, 5.50)
-
 def view():
     conn=sqlite3.connect("lite.db")
     cur=conn.cursor()
@@ -33,5 +31,23 @@ def view():
     rows=cur.fetchall()
     conn.close()
     return rows
+
+def delete(item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    conn.commit()
+    conn.close()
+
+def update(quantity, price, item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity, price, item))
+    conn.commit()
+    conn.close()
+
+insert('Wine Glass', 12, 9.50)
+# delete('Wine Glass')
+update(10, 6, "Wine Glass")
 
 print(view())
