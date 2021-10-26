7881170c5cfadf7a7714608e6a079df725e34cd4
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-26 19:35:23 -0400

    Updated backend.py to improve efficiency of code with methods

--- bookstore_with_classes/backend.py
+++ bookstore_with_classes/backend.py
@@ -5,53 +5,37 @@ class Database:
 
     # Create initial constructor/initalizer for class
     def __init__(self,db):
-        conn=sqlite3.connect(db)
-        cur=conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
-        conn.commit()
-        conn.close()
+        self.conn=sqlite3.connect(db)
+        self.cur=self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+        self.conn.commit()
 
     # Function for adding a book record
     def insert(self,title, author, year, isbn):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title, author, year, isbn,))
-        conn.commit()
-        conn.close()
+        self.cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title, author, year, isbn,))
+        self.conn.commit()
 
     # Function for view all records button
     def view(self):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("SELECT * FROM book")
-        rows=cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT * FROM book")
+        rows=self.cur.fetchall()
         return rows
 
     # Function for searching records
     def search(self,title="",author="",year="",isbn=""):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("SELECT * FROM book where title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
-        rows=cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT * FROM book where title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+        rows=self.cur.fetchall()
         return rows
 
     # Function for deleting a record
     def delete(self,id):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("DELETE FROM book WHERE id=?", (id,))
-        conn.commit()
-        conn.close()
+        self.cur.execute("DELETE FROM book WHERE id=?", (id,))
+        self.conn.commit()
 
     # Function for updating a record
     def update(self,id,title,author,year,isbn):
-        conn=sqlite3.connect("books.db")
-        cur=conn.cursor()
-        cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id,))
-        conn.commit()
-        conn.close()
+        self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id,))
+        self.conn.commit()
 
 # insert("The Earth", "John Smith", 1994, 400392029)
 # delete(3)
