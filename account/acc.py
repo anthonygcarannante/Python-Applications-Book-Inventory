6f0be776504f4c646a1784a4fffbb457e4f82164
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-26 19:56:44 -0400

    Added withdraw, deposit, and updating of bank account text file

--- account/acc.py
+++ account/acc.py
@@ -2,14 +2,26 @@ class Account:
 
     def __init__(self,filepath):
         with open(filepath, 'r') as file:
+            self.filepath=filepath
             # Save value from text file in instance variable (self is object, balance is instance variable stored in object)
             self.balance=int(file.read())
+
+    def withdraw(self, amount):
+        self.balance=self.balance - amount
+
+    def deposit(self, amount):
+        self.balance=self.balance + amount
+
+    def commit(self):
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
+
 
 # Run class, passing the path of the text file
 account=Account("account/balance.txt")
 
-# Print object
-print(account)
-
 # Print instance of balance variable
+print(account.balance) 
+account.withdraw(100)
 print(account.balance)
+account.commit()
