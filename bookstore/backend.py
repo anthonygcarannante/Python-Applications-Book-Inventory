41dff815b99c732c09743fabc06f3f547a0e77c8
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-25 20:58:59 -0400

    Finished backend functionality for searching through SQL database

--- bookstore/backend.py
+++ bookstore/backend.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+# Function for creating database if it doesn't exist
 def connect():
     conn=sqlite3.connect("books.db")
     cur=conn.cursor()
@@ -7,6 +8,7 @@ def connect():
     conn.commit()
     conn.close()
 
+# Function for adding a book record
 def insert(title, author, year, isbn):
     conn=sqlite3.connect("books.db")
     cur=conn.cursor()
@@ -14,6 +16,7 @@ def insert(title, author, year, isbn):
     conn.commit()
     conn.close()
 
+# Function for view all records button
 def view():
     conn=sqlite3.connect("books.db")
     cur=conn.cursor()
@@ -22,6 +25,34 @@ def view():
     conn.close()
     return rows
 
+# Function for searching records
+def search(title="", author="", year="", isbn=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book where title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+# Function for deleting a record
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+# Function for updating a record
+def update(id, title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id,))
+    conn.commit()
+    conn.close()
+
+
 connect()
-insert("The sand", "Tony Buckets", 1994, 10293874)
+# insert("The Earth", "John Smith", 1994, 400392029)
+# delete(3)
+update(4, "The Moon", "John Smooth", "1917", 102394959)
 print(view())
